3d0dac10bcb4ce22694646b98cd8cf38fd5a8e81
ABDELLAH YOUSSFI <abdellah.y.ouahabi@gmail.com>
2023-04-24 17:27:51 +0100

    checking the new 3d module performance

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,12 @@ import MousePad from "./components/MousePad";
 import PicFrame from "./components/PicFrame";
 import Desk from "./components/Desk";
 import Chair from "./components/Chair";
-//import Typing from "./components/sittingChar";
+import Typing from "./components/sittingChar";
 import { HTMLIcon } from "./components/CV/Areas/skills/HTML";
 import { CSSIcon } from "./components/CV/Areas/skills/CSS";
 import { ReactIcon } from "./components/CV/Areas/skills/React";
 import { TSIcon } from "./components/CV/Areas/skills/TS";
-//import Playground from "./components/CV/playground";
+import Playground from "./components/CV/playground";
  
 
 const App = () => {
